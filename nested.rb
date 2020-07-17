bfff5d217a186761279e20119da538ef4ebc5832
Jacob Viti <thevitij64@gmail.com>
2020-07-17 05:44:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages]="Assembly"
+		 newLang=["Assembly"]
+return programmer_hash[:dennis_ritchie][:languages]=newLang
 end
